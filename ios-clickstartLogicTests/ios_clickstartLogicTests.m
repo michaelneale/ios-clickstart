922ccdb6eee09f08ccca61392fa9274c4529a84c
Michael Neale <michael.neale@gmail.com>
2013-03-11 19:57:50 +1100

    added logic tests

---
+++ ios-clickstartLogicTests/ios_clickstartLogicTests.m
@@ -0,0 +1,33 @@
+//
+//  ios_clickstartLogicTests.m
+//  ios-clickstartLogicTests
+//
+//  Created by Michael Neale on 11/03/13.
+//  Copyright (c) 2013 Michael Neale. All rights reserved.
+//
+
+#import "ios_clickstartLogicTests.h"
+
+@implementation ios_clickstartLogicTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STAssertEquals(@"this", @"this", @"Everything is ok");
+//    STFail(@"Unit tests are not implemented yet in ios-clickstartLogicTests");
+}
+
+@end
