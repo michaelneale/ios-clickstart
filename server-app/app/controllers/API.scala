88cf5fc9158b6ed34fbb69697d9059e45e67a30d
Michael Neale <michael.neale@gmail.com>
2013-03-20 17:29:40 +1100

    adding config

--- server-app/app/controllers/API.scala
+++ server-app/app/controllers/API.scala
@@ -3,10 +3,12 @@ package controllers
 import play.api._
 import play.api.libs.json._
 import play.api.mvc._
+import play.api.Play.current
 
 object API extends Controller {
   
   def search(query:String) = Action {    
+    println(Play.application.configuration.getString("couch.db.url"))
     Ok(Json.toJson(Map("result" -> ("data here " + System.currentTimeMillis) )))
   }
 
